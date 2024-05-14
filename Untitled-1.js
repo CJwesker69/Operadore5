532901a54cf5c66539e0bc06a27642b8de78d398
CJwesker69 <128808539+CJwesker69@users.noreply.github.com>
2024-05-14 08:32:36 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,22 @@
+
+var nome = "Iago";
+var idade = 16;
+
+//const
+const PI = 3.14;
+const URL = "www.site.com";
+
+// let
+let saldo = 50;
+let produto = "Produto";
+
+
+
+nome = "Iago"
+Sobrenome = "Daniel"
+
+valor = "100"
+Item = "CD"
+
+dia = "20"
+Horas = "13"
